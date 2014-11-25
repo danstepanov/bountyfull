e9728cfb553b59f446a98a6c2c27768d357f5cc3
Dan Stepanov <dhs149@psu.edu>
2014-11-24 23:31:24 -0500

    initial commit

---
+++ BountyFull/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BountyFull
+//
+//  Created by Daniel Stepanov on 11/24/14.
+//  Copyright (c) 2014 Dan Stepanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
